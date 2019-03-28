02853f513c5b30a43db53a43bf2bb71775da2049
darrion gering and jaime takikawa <darrion@>
2019-03-28 16:17:27 -0700

    Add button and forms to index.html

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  var datingScore = 0;
+
+  if(datingScore > 3 && datingScore < 5) {
+    ("#person1").show();;
+  } elif(datingScore < 7)
+  $("submit-button").submit(function() {
+    alert();
+
+
+  });
+});
